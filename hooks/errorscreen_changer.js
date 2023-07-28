02852f78bd21700a4f6ba5c40fd62def0b0bca03
psolipaPL <126564692+psolipaPL@users.noreply.github.com>
2023-07-28 18:37:40 +0100

    Update errorscreen_changer.js

--- hooks/errorscreen_changer.js
+++ hooks/errorscreen_changer.js
@@ -10,7 +10,7 @@ module.exports = function (context) {
 
   utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile1, errorFileContent1, confs.textToReplace, '');
   errorFileContent1 = utils.readErrorFile(context.opts.projectRoot + confs.androidPath + confs.errorFile1);
-  utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile1, errorFileContent1, scriptToReplace, scriptToReplace + 'document.addEventListener("backbutton", function(e)       {        e.preventDefault();console.log("clicked");    }, false);    }');
+  utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile1, errorFileContent1, scriptToReplace, scriptToReplace + 'document.addEventListener("backbutton", function(e)       {        e.preventDefault();console.log("clicked");    }, false);    ');
   
   utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile2, errorFileContent2, confs.textToReplace, '');
 }
