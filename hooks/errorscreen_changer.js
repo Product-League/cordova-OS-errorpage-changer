da234efcbe59b0d722d270e32ce2f6a984e62052
psolipaPL <126564692+psolipaPL@users.noreply.github.com>
2023-07-28 18:24:06 +0100

    Update errorscreen_changer.js

--- hooks/errorscreen_changer.js
+++ hooks/errorscreen_changer.js
@@ -3,14 +3,14 @@ const utils = require("./utils");
 module.exports = function (context) {
 
   const confs = utils.getConfigs();
-  const scriptToReplace = '})();</script>';
+  const scriptToReplace = 'appendMetaTagAttributes("viewport", "content", ["user-scalable=no", "minimum-scale=1.0"]);';
 
   let errorFileContent1 = utils.readErrorFile(context.opts.projectRoot + confs.androidPath + confs.errorFile1);
   let errorFileContent2 = utils.readErrorFile(context.opts.projectRoot + confs.androidPath + confs.errorFile2);
 
   utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile1, errorFileContent1, confs.textToReplace, '');
   errorFileContent1 = utils.readErrorFile(context.opts.projectRoot + confs.androidPath + confs.errorFile1);
-  utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile1, errorFileContent1, scriptToReplace, 'function onDeviceReady() {            document.addEventListener("backbutton", function(e)       {        e.preventDefault();console.log("clicked");    }, false);    }window.onload = onDeviceReady();' + scriptToReplace);
+  utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile1, errorFileContent1, scriptToReplace, scriptToReplace + 'document.addEventListener("backbutton", function(e)       {        e.preventDefault();console.log("clicked");    }, false);    }');
   
   utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile2, errorFileContent2, confs.textToReplace, '');
 }
